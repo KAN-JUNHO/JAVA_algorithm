48fb89db9b40328d2bc651e4306e8b50d5bfdd39
junho <junho6667@gmail.com>
2021-07-05 18:04:58 +0900

    2021/07/05

---
+++ src/backjoon/입출력과_사칙연산1/A_B.java
@@ -0,0 +1,21 @@
+package backjoon.입출력과_사칙연산1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A_B {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
+
+    }
+}
