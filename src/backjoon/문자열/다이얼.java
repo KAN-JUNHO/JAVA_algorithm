3844ea277832990bfdae635822b097398381a0f6
junho <junho6667@gmail.com>
2021-07-07 15:57:14 +0900

    2021/07/07

--- src/backjoon/문자열/다이얼.java
+++ src/backjoon/문자열/다이얼.java
@@ -11,8 +11,24 @@ public class 다이얼 {
         char[] chars = br.readLine().toCharArray();
         int time=0;
         for(char ch:chars){
-            if(ch>='A' )
+            if(ch>='A' && ch<='C'){
+                time+=3;
+            } else if (ch >= 'D' && ch <= 'F'){
+                time +=4;
+            } else if (ch >= 'G' && ch <= 'I'){
+                time +=5;
+            }else if (ch >= 'J' && ch <= 'L'){
+                time +=6;
+            }else if (ch >= 'M' && ch <= 'O'){
+                time +=7;
+            }else if (ch >= 'P' && ch <= 'S'){
+                time +=8;
+            }else if (ch >='T' && ch <= 'V'){
+                time +=9;
+            }else if (ch >= 'W' && ch <= 'Z') {
+                time += 10;
+            }
         }
-
+        System.out.println(time);
     }
 }
