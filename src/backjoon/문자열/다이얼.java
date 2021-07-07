fa1fb2306e0fb6ed7f8fa1d7b827146ad28f24ab
junho <junho6667@gmail.com>
2021-07-07 15:51:24 +0900

    2021/07/07

---
+++ src/backjoon/문자열/다이얼.java
@@ -0,0 +1,18 @@
+package backjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 다이얼 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        int time=0;
+        for(char ch:chars){
+            if(ch>='A' )
+        }
+
+    }
+}
